2436ee9f61ebde031baabddd2b2f4c185933a035
mrlei <191789784@qq.com>
2019-11-25 12:55:45 +0800

    升级 MybatisPlus 至 3.2.0 版本（移除性能分析插件）

--- src/main/java/com/leigq/www/mybatisplusstudy/config/MyBatisPlusConfig.java
+++ src/main/java/com/leigq/www/mybatisplusstudy/config/MyBatisPlusConfig.java
@@ -4,10 +4,8 @@ import com.baomidou.mybatisplus.core.parser.ISqlParser;
 import com.baomidou.mybatisplus.extension.parsers.BlockAttackSqlParser;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,21 +38,6 @@ public class MyBatisPlusConfig {
         sqlParserList.add(new BlockAttackSqlParser());
         paginationInterceptor.setSqlParserList(sqlParserList);
         return paginationInterceptor;
-    }
-
-    /**
-     * SQL执行效率插件 设置 dev test 环境开启
-     * @author ：LeiGQ <br>
-     * @date ：2019-06-14 10:46 <br>
-     * <p>
-     */
-    @Bean
-    @Profile({"dev","test"})
-    public PerformanceInterceptor performanceInterceptor() {
-        final PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-        performanceInterceptor.setFormat(true);
-        performanceInterceptor.setMaxTime(3000);
-        return new PerformanceInterceptor();
     }
 
     /**
