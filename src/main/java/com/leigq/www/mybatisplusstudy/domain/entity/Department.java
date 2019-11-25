49a6241eb07602ed02559d23acab91ea200bfd55
mrlei <191789784@qq.com>
2019-11-25 11:59:52 +0800

    1、将 Springboot 升级至2.1.2.RELEASE 2、将 tomcat 替换为 undertow 3、去除 swagger2 依赖

--- src/main/java/com/leigq/www/mybatisplusstudy/domain/entity/Department.java
+++ src/main/java/com/leigq/www/mybatisplusstudy/domain/entity/Department.java
@@ -5,8 +5,6 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 
 import java.io.Serializable;
 
@@ -17,7 +15,6 @@ import lombok.EqualsAndHashCode;
  * Created by Mybatis Generator 2019/06/14
  */
 @EqualsAndHashCode(callSuper = true)
-@ApiModel(value = "com.leigq.www.mybatisplusstudy.domain.entity.Department")
 @Data
 @TableName(value = "department")
 public class Department extends Model<Department> implements Serializable {
@@ -25,21 +22,18 @@ public class Department extends Model<Department> implements Serializable {
      * 主键
      */
     @TableId(value = "id", type = IdType.AUTO)
-    @ApiModelProperty(value = "主键")
     private Long id;
 
     /**
      * 名称
      */
     @TableField(value = "name")
-    @ApiModelProperty(value = "名称")
     private String name;
 
     /**
      * 员工数量
      */
     @TableField(value = "staff_num")
-    @ApiModelProperty(value = "员工数量")
     private Short staffNum;
 
     private static final long serialVersionUID = 1L;
